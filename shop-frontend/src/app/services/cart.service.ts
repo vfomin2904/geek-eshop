9d9344494c5b6f6d6333767d61980845a48a9a72
vfomin2904 <vova-fomin97@mail.ru>
2021-10-07 21:44:52 +0300

    cart

---
+++ shop-frontend/src/app/services/cart.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AddLineItemDto} from "../model/add-line-item-dto";
+import {Observable} from "rxjs";
+import {AllCartDto} from "../model/all-cart-dto";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor(private http: HttpClient) { }
+
+  public findAll() : Observable<AllCartDto> {
+    return this.http.get<AllCartDto>('/api/v1/cart/all');
+  }
+
+  public addToCart(dto: AddLineItemDto) {
+    return this.http.post('/api/v1/cart', dto)
+  }
+}
